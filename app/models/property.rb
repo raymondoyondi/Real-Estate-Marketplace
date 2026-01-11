d58d2348d6d582764e1581e891a62667ee9a6a9f
Raymond Oyondi <raymondoyondi@gmail.com>
2026-01-11 11:18:52 -0600

    Update property.rb

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,9 +1,9 @@
 class Property < ApplicationRecord
   belongs_to :company
   validates_presence_of :address, :owner, :price, :size
-  enum basement: [:No, :Yes]
-  enum style: [:townhouse, :apartment]
-  enum floors: [:"1", :"2", :"3", :"4", :"5+"]
+  enum :basement, [:No, :Yes]
+  enum :style, [:townhouse, :apartment]
+  enum :floors, [:"1", :"2", :"3", :"4", :"5+"]
   # has_many_attached :images
   mount_uploader :image, ImageUploader
   serialize :image, JSON
