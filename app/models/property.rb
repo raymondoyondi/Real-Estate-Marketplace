1ee14b78a8f9c09fcbd579150ad0c441d360360a
Raymond Oyondi <raymondoyondi@gmail.com>
2026-01-11 11:29:20 -0600

    Update property.rb

--- app/models/property.rb
+++ app/models/property.rb
@@ -6,5 +6,5 @@ class Property < ApplicationRecord
   enum :floors, [:"1", :"2", :"3", :"4", :"5+"]
   # has_many_attached :images
   mount_uploader :image, ImageUploader
-  serialize :image, JSON
+  serialize :image, coder: JSON
 end
